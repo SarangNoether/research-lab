8f6290bd2a913d6a77f791db1be50bf72f430819
Sarang Noether <32460187+SarangNoether@users.noreply.github.com>
2018-10-15 15:17:41 +0000

    Unit test cleanup

--- source-code/pyruff/test.py
+++ source-code/pyruff/test.py
@@ -7,59 +7,106 @@ import pyruff
 import multisig
 
 class TestPyRuff(unittest.TestCase):
-    def test_verify1(self):
+    # Test verify1 using a 2x2 matrix
+    def test_verify1_2x2(self):
         b = []
         b.append([Scalar(1),Scalar(0)])
         b.append([Scalar(0),Scalar(1)])
-        r = Scalar(1)
+        r = random_scalar()
         proof1 = pyruff.prove1(b,r)
 
         B = pyruff.matrix_commit(b,r)
         pyruff.verify1(B,proof1)
 
-    def test_verify2_1(self):
-        r = Scalar(1)
-        s = Scalar(1)
+    # Test verify1 using using a 3x4 matrix
+    def test_verify1_3x4(self):
+        b = []
+        b.append([Scalar(0),Scalar(1),Scalar(0)])
+        b.append([Scalar(0),Scalar(1),Scalar(0)])
+        b.append([Scalar(1),Scalar(0),Scalar(0)])
+        b.append([Scalar(0),Scalar(0),Scalar(1)])
+        r = random_scalar()
+        proof1 = pyruff.prove1(b,r)
+
+        B = pyruff.matrix_commit(b,r)
+        pyruff.verify1(B,proof1)
+
+    # Test verify1 using malformed 2x2 matrices
+    def test_verify1_2x2_bad(self):
+        # This matrix has duplicate 1 entries
+        b = []
+        b.append([Scalar(1),Scalar(0)])
+        b.append([Scalar(1),Scalar(1)])
+        r = random_scalar()
+        proof1 = pyruff.prove1(b,r)
+
+        B = pyruff.matrix_commit(b,r)
+        with self.assertRaises(ArithmeticError):
+            pyruff.verify1(B,proof1)
+
+        # This matrix has too few 1 entries
+        b = []
+        b.append([Scalar(0),Scalar(0)])
+        b.append([Scalar(1),Scalar(1)])
+        r = random_scalar()
+        proof1 = pyruff.prove1(b,r)
+
+        B = pyruff.matrix_commit(b,r)
+        with self.assertRaises(ArithmeticError):
+            pyruff.verify1(B,proof1)
+
+    # Test verify2 using 2 commitments
+    def test_verify2_2(self):
+        r = random_scalar()
+        s = random_scalar()
         CO = []
         CO.append(pyruff.elgamal_commit(Scalar(0),r))
         CO.append(pyruff.elgamal_commit(Scalar(1),s))
-        ii = 0
         base = 2
         exponent = 1
+        
+        # This is the correct secret index
+        ii = 0
         proof2 = pyruff.prove2(CO,ii,r,base,exponent)
         pyruff.verify2(base,proof2,CO)
 
-    def test_verify2_2(self):
-        r = Scalar(1)
-        CO = []
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(0),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        CO.append(pyruff.elgamal_commit(Scalar(1),Scalar(1)))
-        ii = 5
-        base = 3
-        exponent = 2
-        proof2 = pyruff.prove2(CO,ii,r,base,exponent)
-        pyruff.verify2(base,proof2,CO)
-
-    def test_verify2_3(self):
-        r = Scalar(1)
-        for k in range(1,4):
-            CO = []
-            for i in range(2**k):
-                CO.append(pyruff.elgamal_commit(Scalar(i),Scalar(1)))
-            ii = 0
-            base = 2
-            exponent = k
-            proof2 = pyruff.prove2(CO,ii,r,base,exponent)
+        # This is the wrong secret index
+        ii = 1
+        proof2 = pyruff.prove2(CO,ii,s,base,exponent)
+        with self.assertRaises(ArithmeticError):
             pyruff.verify2(base,proof2,CO)
 
+    # Test verify2 using 9 commitments
+    def test_verify2_9(self):
+        r = random_scalar()
+        s = random_scalar()
+        CO = []
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(Scalar(0),r))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),random_scalar()))
+        CO.append(pyruff.elgamal_commit(random_scalar(),s))
+        base = 3
+        exponent = 2
+
+        # This is the correct secret index
+        ii = 5
+        proof2 = pyruff.prove2(CO,ii,r,base,exponent)
+        pyruff.verify2(base,proof2,CO)
+
+        # This is the wrong secret index
+        ii = 8
+        proof2 = pyruff.prove2(CO,ii,s,base,exponent)
+        with self.assertRaises(ArithmeticError):
+            pyruff.verify2(base,proof2,CO)
+
+    # Perform a complete spend test
     def test_2_1_1(self):
+        return # NOTE: this test has been manually disabled
         base = 2
         exponent = 1
         inputs = 1
@@ -125,24 +172,28 @@ class TestPyRuff(unittest.TestCase):
         pyruff.verify(sp.KI,sp.PK,sp.CO,sig.CO1,sp.m,sig)
 
 class TestMultisig(unittest.TestCase):
+    # Verify a signature with 1 key
     def test_1(self):
         x = [random_scalar()]*1
         X = [G*i for i in x]
         m = hash_to_scalar('test message')
         multisig.verify(m,X,multisig.sign(m,x))
 
+    # Verify a signature with 2 keys
     def test_2(self):
         x = [random_scalar()]*2
         X = [G*i for i in x]
         m = hash_to_scalar('test message')
         multisig.verify(m,X,multisig.sign(m,x))
 
+    # Verify a signature with 2 keys where the public keys are in a different order
     def test_2_order(self):
         x = [random_scalar()]*2
         X = list(reversed([G*i for i in x]))
         m = hash_to_scalar('test message')
         multisig.verify(m,X,multisig.sign(m,x))
 
+    # Test a bad signature with random public keys
     def test_2_bad(self):
         x = [random_scalar()]*2
         X = [random_point() for i in x]
@@ -151,18 +202,21 @@ class TestMultisig(unittest.TestCase):
             multisig.verify(m,X,multisig.sign(m,x))
 
 class TestMatrixCommit(unittest.TestCase):
+    # Commit to a 1x1 matrix
     def test_1_1(self):
         m = [[random_scalar()]]
         r = random_scalar()
         result = hash_to_point('pyruff 0 0')*m[0][0] + G*r
         self.assertEqual(pyruff.matrix_commit(m,r),result)
 
+    # Commit to a 2x1 matrix
     def test_2_1(self):
         m = [[random_scalar()],[random_scalar()]]
         r = random_scalar()
         result = hash_to_point('pyruff 0 0')*m[0][0] + hash_to_point('pyruff 1 0')*m[1][0] + G*r
         self.assertEqual(pyruff.matrix_commit(m,r),result)
 
+    # Commit to a 1x2 matrix
     def test_1_2(self):
         m = [[random_scalar(),random_scalar()]]
         r = random_scalar()
@@ -170,7 +224,8 @@ class TestMatrixCommit(unittest.TestCase):
         self.assertEqual(pyruff.matrix_commit(m,r),result)
 
 class TestProduct(unittest.TestCase):
-    def test_1(self):
+    # Compute coefficients for degree-0 cases
+    def test_degree_0(self):
         c = [random_scalar()]
         d = [Scalar(0)]
         result = [Scalar(0)]
@@ -182,14 +237,16 @@ class TestProduct(unittest.TestCase):
         self.assertEqual(pyruff.product(c,d),result)
         self.assertEqual(pyruff.product(d,c),result)
 
-    def test_2(self):
+    # Compute coefficients for degree-1 polynomial
+    def test_degree_1(self):
         c = [random_scalar()]*2
         d = [random_scalar()]*2
         result = [c[0]*d[0],c[0]*d[1]+c[1]*d[0],c[1]*d[1]]
         self.assertEqual(pyruff.product(c,d),result)
         self.assertEqual(pyruff.product(d,c),result)
 
-    def test_3(self):
+    # Compute coefficients for a degree-1 times a degree-2
+    def test_degree_1_2(self):
         c = [random_scalar()]*2
         d = [random_scalar()]*3
         result = [c[0]*d[0],c[0]*d[1]+c[1]*d[0],c[1]*d[1]+c[0]*d[2],c[1]*d[2],Scalar(0)]
